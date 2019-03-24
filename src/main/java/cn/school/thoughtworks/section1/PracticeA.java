f84e9bf2b880d840b8f65924dcfd13e5852ec423
cas <595972434@qq.com>
2019-03-24 18:18:32 +0800

    session1-A代码运行成功

--- src/main/java/cn/school/thoughtworks/section1/PracticeA.java
+++ src/main/java/cn/school/thoughtworks/section1/PracticeA.java
@@ -1,14 +1,19 @@
 package cn.school.thoughtworks.section1;
 
 import java.util.List;
-import java.util.Arrays;
+import java.util.ArrayList;
 import java.util.List;
 
 public class PracticeA {
     List<String> collectSameElements(List<String> collection1, List<String> collection2) {
-
-        List<String> result = Arrays.asList("a", "e", "f", "d");
-        List<String> result111 = Arrays.asList("aaaaa", "e", "f", "d");
+        List<String> result=new ArrayList<String>();
+        for(int i=0;i<collection1.size();i++)
+        {
+            if(collection2.contains(collection1.get(i)))
+            {
+                result.add(collection1.get(i));
+            }
+        }
         return result;
     }
 
