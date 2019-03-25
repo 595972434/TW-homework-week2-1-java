b86ad8301793d8ae03712ae865805adb4a9e23ed
cas <595972434@qq.com>
2019-03-25 11:13:10 +0800

    session3-A测试通过

--- src/main/java/cn/school/thoughtworks/section3/PracticeA.java
+++ src/main/java/cn/school/thoughtworks/section3/PracticeA.java
@@ -1,12 +1,21 @@
 package cn.school.thoughtworks.section3;
 
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 public class PracticeA {
     Map<String,Integer> createUpdatedCollection(Map<String,Integer> collectionA, Map<String,List<String>> object) {
+        Map<String, Integer> result = new HashMap<String, Integer>();
+        for (String key : collectionA.keySet()) {
+            if(object.get("value").contains(key)){
+                result.put(key,collectionA.get(key)-1);
+            }
+            else{
+                result.put(key,collectionA.get(key));
+            }
+        }
 
-
-        return null;
+        return result;
     }
 }
